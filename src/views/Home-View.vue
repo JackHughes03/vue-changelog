7d1c18b37565905fefae50c1ca6841feb5c79ea4
JackHughes03 <jwhughes03@yahoo.com>
2024-09-30 21:41:39 +0100

    Setup files

---
+++ src/views/Home-View.vue
@@ -0,0 +1,16 @@
+<script>
+// Components
+import Navigation from '@/components/Navigation-Component.vue'
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<template>
+  <Navigation />
+</template>
+
+<style></style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './index.html',
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
