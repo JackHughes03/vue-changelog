59224fa6b1ff487da032d628d234ffbbb4f31e3b
JackHughes03 <jwhughes03@yahoo.com>
2024-10-01 22:06:13 +0100

    Added demo component to showcase how it works.

---
+++ components/Header-Component.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="mx-auto max-w-2xl p-4 py-20 flex flex-col text-center gap-2 items-center justify-center">
+        <h1 class="text-3xl">Lorem ipsum dolor</h1>
+        <p class="text-sm">Dolor ipsum adipisicing ipsum ad quis non esse enim aute. Nulla magna commodo dolore laboris pariatur non voluptate anim consectetur. Dolore elit occaecat occaecat in elit culpa officia excepteur do proident velit nulla cupidatat ad.</p>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial />
+  <header class="bg-black/80 h-full w-full m-0 p-0 text-white">
+    <Header-Component />
+  </header>
 </template>
 
 <script>
